f20711e8e42a15e387e88f9fb031330cf11258ab
Xytx2004 <147287148+Xytx2004@users.noreply.github.com>
2023-11-27 21:18:11 +0800

    2023.11.27第一次培训作业

--- Assignment01/Program.cs
+++ Assignment01/Program.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+﻿/*using Microsoft.AspNetCore.Cryptography.KeyDerivation;
 using System.Diagnostics;
 using System.Security.Cryptography;
 using System.Text;
@@ -44,4 +44,87 @@ static string GenerateEncryptedPassword(string originalPassword)
         numBytesRequested: 64);
     string password = Convert.ToBase64String(hashedPassword.Concat(salt).ToArray());
     return password;
+}*/
+
+
+
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+
+class Program
+{
+    static void Main()
+    {
+        Stopwatch Time = new Stopwatch();
+
+        // 创建一个用于写入的文件流
+        var writer = File.Open("generated_password.csv", FileMode.CreateNew);
+
+        // 写入文件标题（UTF-8格式）
+        writer.Write(Encoding.UTF8.GetBytes("学号,密码\n"));
+
+        // 读取所有行数据到字符串数组
+        string[] lines = File.ReadAllLines("input.txt");
+
+        Time.Start();
+
+        int Batchprocessing = 300;
+
+        // 并行处理数据
+        Parallel.For(0, lines.Length / Batchprocessing, (i) =>
+        {
+            // 处理当前批次的数据
+            ProcessBatch(lines.Skip(i * Batchprocessing).Take(Batchprocessing).ToArray(), writer);
+        });
+
+        writer.Flush();
+
+        writer.Close();
+
+        Console.WriteLine($"已处理{lines.Length}条数据，耗时：{Time.Elapsed.TotalSeconds}s");
+    }
+
+    // 处理批次数据
+    static void ProcessBatch(string[] batch, FileStream writer)
+    {
+        // 遍历处理当前批次的每一行数据
+        foreach (var line in batch)
+        {
+            // 拆分行数据
+            string[] parts = line.Split(" ");
+
+            // 生成加密密码
+            string newLine = $"{parts[1]},{GenerateEncryptedPassword(parts[1])}\n";
+
+            // 写入数据到文件
+            lock (writer)
+            {
+                writer.Write(Encoding.UTF8.GetBytes(newLine));
+            }
+        }
+    }
+
+    static string GenerateEncryptedPassword(string originalPassword)
+    {
+        // 生成随机盐值
+        byte[] salt = RandomNumberGenerator.GetBytes(64);
+
+        // 生成采用PBKDF2算法加密后的64字节密码+64字节盐值的Base64字符串
+        byte[] hashedPassword = KeyDerivation.Pbkdf2(
+            password: originalPassword,
+            salt: salt,
+            prf: KeyDerivationPrf.HMACSHA256,
+            iterationCount: 10000,
+            numBytesRequested: 64);
+
+        // 拼接转换为Base64字符串
+        string password = Convert.ToBase64String(hashedPassword.Concat(salt).ToArray());
+        return password;
+    }
 }
