d6f8f13e4ec61715d48f616b01935964b29f0b8e
HHao <work@hhao.wang>
2023-12-04 16:39:12 +0800

    第二次作业

--- Assignment01/Program.cs
+++ Assignment01/Program.cs
@@ -3,35 +3,57 @@ using System.Diagnostics;
 using System.Security.Cryptography;
 using System.Text;
 
-Stopwatch stopwatch = new Stopwatch();
-var writer = File.Open("generated_password.csv", FileMode.CreateNew);
-writer.Write(Encoding.UTF8.GetBytes("学号,密码\n"));
+Stopwatch stopwatch = new();
+FileStream writer = File.Open("generated_password.csv", FileMode.Create);
+writer.Write("学号,密码\n"u8);
 string[] lines = File.ReadAllLines("input.txt");
+
+string[][] chunks = lines.Chunk(1000).ToArray();
+
 stopwatch.Start();
 
-int n = 0;
-foreach (var line in lines)
+int threadCount = 0;
+int i = 0;
+
+while (true)
 {
-    string[] parts = line.Split(" ");
-    string newLine = $"{parts[1]},{GenerateEncryptedPassword(parts[1])}\n";
-    writer.Write(Encoding.UTF8.GetBytes(newLine));
-
-    if (n % 500 == 0)
+    if (threadCount >= 8)
     {
-        writer.Flush();
-        Console.WriteLine($"已处理{n}条数据，耗时：{stopwatch.Elapsed.TotalSeconds}s");
+        Thread.Sleep(500);
+        continue;
     }
-    n++;
+
+    if (i >= chunks.Length)
+    {
+        break;
+    }
+
+    Thread thread = new(Generate);
+    thread.Start(chunks[i]);
+    i++;
+    threadCount++;
 }
-writer.Flush();
-writer.Close();
 
+void Generate(object? partialLinesObject)
+{
+    string[] partialLines = (string[])partialLinesObject!;
+    foreach (var line in partialLines)
+    {
+        string[] parts = line.Split(" ");
+        string newLine = $"{parts[1]},{GenerateEncryptedPassword(parts[1])}\n";
 
-/// <summary>
-/// 生成加密后的密码
-/// </summary>
-/// <param name="originalPassword">原始密码</param>
-/// <returns>加密后的密码</returns>
+        lock (writer)
+        {
+            writer.Write(Encoding.UTF8.GetBytes(newLine));
+        }
+    }
+
+    writer.Flush();
+    Console.WriteLine($"已处理{partialLines.Length}条数据，当前时间：{stopwatch.Elapsed.TotalSeconds}s");
+
+    threadCount--;
+}
+
 static string GenerateEncryptedPassword(string originalPassword)
 {
     //生成采用PBKDF2算法加密后的64字节密码+64字节盐值的Base64字符串
---
+++ Assignment02/Services/OrderService.cs
@@ -0,0 +1,21 @@
+﻿using Assignment02.Dto;
+
+namespace Assignment02.Services;
+
+public class OrderService
+{
+    private static List<OrderDto> _ordersList = new();
+    private readonly GoodsService _goodsService;
+    private readonly UserService _userService;
+
+    public OrderService()
+    {
+        _goodsService = new();
+        _userService = new();
+    }
+
+    public async Task<bool> PlacingAnOrder(OrderDto order)
+    {
+        throw new NotImplementedException();
+    }
+}
